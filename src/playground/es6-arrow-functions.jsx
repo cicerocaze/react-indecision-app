c8cfa3bde10be64f77620db324b8cb863a11d099
Cicero Caze <cicerocaze@gmail.com>
2018-02-16 18:25:07 -0200

    Added new jsx file to test arrow functions

--- src/playground/es6-arrow-functions.jsx
+++ src/playground/es6-arrow-functions.jsx
@@ -16,10 +16,10 @@ console.log(squareArrow(4)); */
 //Create regular arrow functions
 //Create arrow function using shorthand syntax
 
-const getFirstName = fullName => {
+const getFirstName = (fullName) => {
     return fullName.split(' ')[0];
-};
+}
 console.log(getFirstName('Mike Smith'));
 
-const getLastName = fullName => fullName.split(' ')[1];
+const getLastName = (fullName) => fullName.split(' ')[1];
 console.log(getLastName('Mike Smith'));
