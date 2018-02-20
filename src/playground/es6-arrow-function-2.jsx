9431b929a6373768fb977e64a2b646d4220e3edd
Cicero Caze <cicerocaze@gmail.com>
2018-02-20 18:41:39 -0300

    New arrow functions

--- src/playground/es6-arrow-function-2.jsx
+++ src/playground/es6-arrow-function-2.jsx
@@ -1,1 +1,19 @@
-// arguments
+// arguments objetcts - no longer bound with arrow functions
+
+const add = (a, b) => {
+    //console.log(arguments);
+    return a + b;
+}
+console.log(add(55, 1, 1001));
+
+
+// this keyword - no longer bound
+const user = {
+    name: 'Andrew',
+    cities: ['Philadelphia', 'New York', 'Dublin'],
+    printPlacesLived: function () {
+        console.log(this.name);
+        console.log(this.cities);
+    }
+};
+user.printPlacesLived();
